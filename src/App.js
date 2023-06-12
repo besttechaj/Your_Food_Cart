a86147f2411d2be3c1ccba872db958c55dd8265e
Ajay Mishra <asap2656@gmail.com>
2023-06-12 15:23:55 +0530

    Added react-router-dom and working on user's index page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './screens/Home';
+import { Routes, Route } from 'react-router-dom';
+import Login from './screens/Login';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //HERE WE HAVE WRAPPED THE BROWSER ROUTER(Router) IN index.js file
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/screens/Home.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <div
+          class='card mt-3'
+          style={{
+            width: '18rem',
+            maxHeight: '360px',
+            backgroundColor: 'gray',
+            color: 'white',
+          }}
+        >
+          <img src='...' class='card-img-top' alt='...' />
+          <div class='card-body'>
+            <h5 class='card-title'>Card title</h5>
+            <p class='card-text'>I am a new cart item</p>
+            <div
+              className='container  w-100'
+              style={{ backgroundColor: 'gray' }}
+            >
+              <select className='m-2 h-100  rounded' style={{ color: 'red' }}>
+                {/* syntax-> Array.from(OBJECT,MAPfunction define) //return an
+                array */}
+                {Array.from(Array(6), (ele, i) => {
+                  return (
+                    <option key={i + 1} value={i + 1}>
+                      {i + 1}
+                    </option>
+                  );
+                })}
+              </select>
+              {/* Another select for volume */}
+              <select className='m-2 h-100  rounded' style={{ color: 'red' }}>
+                <option value='half'>Half</option>
+                <option value='full'>Full</option>
+              </select>
+              <div className='d-inline h-100 fs-5'>$Total Price</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
